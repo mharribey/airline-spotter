c44cc3da935d0291a95dfbe46e6bb88709e2d489
mharribey <mathieuharribey@hotmail.fr>
2018-04-03 17:02:19 +0200

    did slider

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 /* CHANGE RATE OF VIDEO */
 
 var vid = document.getElementById("video-loop");
+
+var index = document.querySelector(".container-index");
+var about = document.querySelector(".container-about");
+
 var sPath = window.location.pathname;
 var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
 
@@ -8,12 +12,50 @@ if(sPage == "index.html"){
   vid.playbackRate = 0.5;
 
   vid.addEventListener("mouseover",function(){
-    console.log(vid.playbackRate);
     vid.playbackRate = 2;
   });
 
   vid.addEventListener("mouseout",function(){
-    console.log(vid.playbackRate);
     vid.playbackRate = 0.5;
   });
 }
+
+/* SLIDE */
+
+var btn = document.getElementsByClassName('bottom-btn');
+
+document.addEventListener("DOMContentLoaded",()=>{
+  setTimeout(function(){
+    if(sPage == "index.html"){
+      index.classList.remove("left-start");
+      index.classList.add("is-ready");
+    } else {
+      about.classList.remove("right-start");
+      about.classList.add("is-ready");
+    }
+  },100);
+});
+
+Array.from(btn).forEach(btn=>{
+  btn.addEventListener("click",()=>{
+    if(sPage == "index.html"){
+      index.classList.add("blur");
+      index.classList.add('right-remove');
+      setTimeout(function(){
+        index.classList.remove('is-ready');
+        index.classList.remove('is-ready');
+      },1700);
+
+    } else {
+      about.classList.add("blur");
+      about.classList.add('left-remove');
+      setTimeout(function(){
+        about.classList.remove('is-ready');
+        about.classList.remove('is-ready');
+      },1700);
+    }
+    setTimeout(function(){
+      window.location.href = btn.id+'.html';
+    },500);
+  });
+});
